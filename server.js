e592480635caab68aeb45901d2f2253622d9d91b
David Wee <rook2pawn@gmail.com>
2014-06-04 12:08:11 -0700

    token verification

--- server.js
+++ server.js
@@ -11,16 +11,12 @@ app.use(bodyParser())
 var server = http.createServer(app);
 var request = require('request');
 var response = require('response');
+
+var prod = {
+    url:'https://api.authy.com',
+    key:'f6e1b0c0898c4abba10b002034220fa3'
+}
 app.post('/auth/phone',function(req,res) {
-    var test = {
-        url:'http://sandbox-api.authy.com',
-        key:'591b17780a53fb9872ec1f17f49e4fff'
-    }
-    var prod = {
-        url:'https://api.authy.com',
-        key:'f6e1b0c0898c4abba10b002034220fa3'
-    }
-    var testurl = test.url+'/protected/json/phones/verification/start?api_key='+test.key
     var produrl = prod.url+'/protected/json/phones/verification/start?api_key='+prod.key
     request.post({url:produrl,json:true,body:qs.stringify(req.body)},function(err,resp,body) {
         if (body.success === true) {
@@ -30,5 +26,16 @@ app.post('/auth/phone',function(req,res) {
         }
     });
 })
+app.post('/auth/phone/validate',function(req,res) {
+    var obj = {api_key:prod.key,phone_number:req.body.phone_number,country_code:req.body.country_code,verification_code:req.body.token}
+    var produrl = prod.url+'/protected/json/phones/verification/check'
+    request.get({url:produrl,qs:obj,json:true},function(err,resp,body) {
+        if (body.success === true) {
+            response.json({result:'success'}).pipe(res)
+        } else  {
+            response.json({result:'error'}).status(400).pipe(res)
+        }
+    })
+});
 server.listen(5150)
 
